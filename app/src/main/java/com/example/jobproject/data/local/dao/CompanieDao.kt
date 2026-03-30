7da61f9579533825563e08c655d16bb8619346cd
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-03-30 22:45:46 +0200

    Kreirane operacije u CompanieDao interfejsu

--- app/src/main/java/com/example/jobproject/data/local/dao/CompanieDao.kt
+++ app/src/main/java/com/example/jobproject/data/local/dao/CompanieDao.kt
@@ -2,9 +2,12 @@ package com.example.jobproject.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.jobproject.data.local.models.Companie
+import com.example.jobproject.data.local.models.Industry
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,7 +16,17 @@ interface CompanieDao {
     @Query("SELECT * FROM companies")
     fun getAllCompanies(): Flow<List<Companie>>
 
+    @Query("SELECT * FROM companies WHERE userId = :userId")
+    fun getCompanieById(userId: Int): Flow<Companie?>
 
+    @Query("SELECT * FROM companies WHERE industry = :industry")
+    fun getCompaniesByIndustry(industry: Industry): Flow<List<Companie>>
+
+    @Query("SELECT * FROM companies WHERE companyName LIKE '%' || :query || '%'")
+    fun searchCompaniesByName(query: String): Flow<List<Companie>>
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertCompani(companie: Companie)
 
     @Update
     suspend fun updateCompanie(companie: Companie)
