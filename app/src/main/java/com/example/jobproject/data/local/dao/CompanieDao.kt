b9f0393609d9d64e102ef6c2f4b3c0f261f1e14e
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-03-30 22:38:05 +0200

    Kreirana enum klasa sa kategorijama industrija

--- app/src/main/java/com/example/jobproject/data/local/dao/CompanieDao.kt
+++ app/src/main/java/com/example/jobproject/data/local/dao/CompanieDao.kt
@@ -1,4 +1,23 @@
 package com.example.jobproject.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import com.example.jobproject.data.local.models.Companie
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface CompanieDao {
+
+    @Query("SELECT * FROM companies")
+    fun getAllCompanies(): Flow<List<Companie>>
+
+
+
+    @Update
+    suspend fun updateCompanie(companie: Companie)
+
+    @Delete
+    suspend fun deleteCompanie(companie: Companie)
 }
---
+++ app/src/main/java/com/example/jobproject/data/local/models/Industry.kt
@@ -0,0 +1,14 @@
+package com.example.jobproject.data.local.models
+
+enum class Industry {
+    TECHNOLOGY,
+    HEALTHCARE,
+    FINANCE,
+    EDUCATION,
+    MANUFACTURING,
+    RETAIL,
+    CONSTRUCTION,
+    MARKETING,
+    LEGAL,
+    OTHER
+}
